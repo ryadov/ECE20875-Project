63660633e85b84ea4add40bd1f23b474bac12855
smahir <smahir@purdue.edu>
2022-04-22 22:48:52 -0400

    what i have so far for part 2

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
 
@@ -18,7 +19,7 @@ def getData():
     dataset_1['Williamsburg Bridge'] = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',', '', regex=True))
     dataset_1['Total'] = pandas.to_numeric(dataset_1['Total'].replace(',', '', regex=True))
     dataset_1['Precipitation'] = pandas.to_numeric(dataset_1['Precipitation'].replace(',', '', regex=True))
-    #print(dataset_1.to_string())  # This line will print out your data
+    # print(dataset_1.to_string())  # This line will print out your data
     return dataset_1
 
 
@@ -28,8 +29,10 @@ manhattan = list(getData()["Manhattan Bridge"])
 williamsburg = list(getData()["Williamsburg Bridge"])
 queensboro = list(getData()["Queensboro Bridge"])
 totalTraffic = list(getData()["Total"])
+tempLow = list(getData()["Low Temp"])
+tempHigh = list(getData()["High Temp"])
 precipitation = list(getData()["Precipitation"])
-#print(brooklyn)
+# print(brooklyn)
 X = np.array([brooklyn, manhattan, williamsburg, queensboro]).T
 Y = np.array(totalTraffic).T
 
@@ -89,9 +92,39 @@ print(
     + ", which yields an MSE of "
     + str(MSE_best)
 )
-print(f"The equation is {model_best.coef_[0]} x1 + {model_best.coef_[1]} x2 + {model_best.coef_[2]} x3 + {model_best.coef_[3]} x4 \n where x1 = brooklyn, x2 = manhattan, x3 = williamsburg and x4 = queensboro")
+print(
+    f"The equation is {model_best.coef_[0]} x1 + {model_best.coef_[1]} x2 + {model_best.coef_[2]} x3 + {model_best.coef_[3]} x4 \n where x1 = brooklyn, x2 = manhattan, x3 = williamsburg and x4 = queensboro")
 # getData()
 # print(brooklyn)
 # print(manhattan)
 # print(williamsburg)
 # print(queensboro)
+
+
+####################PART TWO STARTS HERE#####################
+
+X2 = np.array([tempHigh, tempLow, precipitation]).T
+Y2 = totalTraffic
+degrees = [1, 2, 3, 4, 5]
+
+
+def least_squares(X, y):
+    X = np.array(X)
+    y = np.array(y)
+    B = np.linalg.lstsq(X, y, rcond=None)
+    return B[0]
+
+
+paramFits = []
+for i in degrees:
+    # paramFits.append(feature_matrix(data, degrees))
+    paramFits.append(least_squares(X2, Y2))
+
+plt.plot(X2, np.dot(X2, paramFits[0]), color="red", label="d1")
+plt.plot(X2, np.dot(X2, paramFits[1]), color="purple", label="d2")
+plt.plot(X2, np.dot(X2, paramFits[2]), color="blue", label="d3")
+plt.plot(X2, np.dot(X2, paramFits[3]), color="red", label="d4")
+plt.plot(X2, np.dot(X2, paramFits[4]), color="black", label="d5")
+plt.legend(loc="upper left")
+plt.show()
+print(paramFits)
